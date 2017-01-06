aaf72805765619b0b683e8840a931e6ac7d053aa
Adrian Mowat <adrian.mowat@gmail.com>
2017-01-06 23:53:27 +0000

    Fixed minor speech timing flaw

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,12 @@
-PROJECT_HOME=$( cd $(dirname $0)/..; pwd )
-ZIPFILE=$PROJECT_HOME/dist/commandoxfit.src.zip
+#!/usr/bin/env bash
 
-echo "Pushing $(basename $ZIPFILE) to s3"
-s3cmd put $ZIPFILE s3://commandoxfit
+if [[ -z "$AWS_ACCESS_KEY_ID" || -z "$AWS_SECRET_ACCESS_KEY" ]]; then
+  echo "Please export AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY" >&2
+  exit 1
+fi
+
+node-lambda deploy \
+  --environment production \
+  --region eu-west-1 \
+  --functionName "arn:aws:lambda:eu-west-1:525027603511:function:commandoXFitClasses" \
+  --role "arn:aws:iam::525027603511:role/service-role/commandoLambda"
